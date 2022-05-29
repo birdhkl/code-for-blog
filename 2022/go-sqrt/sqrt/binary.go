8464c80c29c3d025dc3ab61385bda0b59b82993e
何康乐 <15967143416@163.com>
2022-05-29 12:11:32 +0800

    blog(sqrt): 2022

--- 2022/go-sqrt/sqrt/binary.go
+++ 2022/go-sqrt/sqrt/binary.go
@@ -10,6 +10,9 @@ type SqrtValue interface {
 	~float32 | ~float64
 }
 
+// BinarySqrt
+// float可以精确到有效数字8位， double到了17位
+// 精度最好低于数据类型(float)最大精度一个数量级，否则会由于 (lo+hi)/2 一直等于 lo 而进入死循环
 func BinarySqrt[T SqrtValue](value T) T {
 	if value < 0 {
 		panic(fmt.Sprintf("BinarySqrt Negative Value %f", value))
@@ -19,9 +22,9 @@ func BinarySqrt[T SqrtValue](value T) T {
 	}
 	min := T(0.0)
 	max := value
-	const SquareRootPrecise = 0.00000001
-	for min+SquareRootPrecise < max {
-		mid := min + (max-min)/2.0
+	const SquareRootPrecise = 10e-6
+	for (max - min) > SquareRootPrecise {
+		mid := min + (max-min)/2.
 		delta := mid*mid - value
 		if -ResultPrecise <= delta && delta <= ResultPrecise {
 			min = mid
